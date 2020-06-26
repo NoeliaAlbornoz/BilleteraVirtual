0bb30481467fb1a06f96212650b140ea588b84cd
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-26 10:18:10 -0300

    Se hicieron comentarios en pseudocódigo para organizar la funcionalidad de los webmethods.

---
+++ src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
@@ -0,0 +1,24 @@
+package ar.com.ada.api.billeteravirtual.controllers;
+
+public class BilleteraController {
+
+    /* WEB Method 1: consultar saldo: GET
+                    URL: billeteras/{id}/saldos
+    WEB Method 2: cargar saldo: POST
+                    URL: billeteras/{id}/recargas
+                    requestBody: {
+                        "moneda":
+                        "importe":
+                    }
+    WEB Method 3: enviar saldo: POST
+                    URL: billeteras/{id}/envios
+                    requestBody: {
+                        "moneda":
+                        "importe":
+                        "email":
+                        "motivo":
+                        "detalleDelMotivo":
+                    } 
+    */
+
+}
