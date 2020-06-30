c100379eba833171d3c76a6ac6b8e974007274c9
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-06-30 06:55:19 -0300

    Encriptamos la contraseña. Creamos en BilleteraService un método que nos permita grabar una billetera. En UsuarioService creamos una billetera y dos tipos de cuentas (pesos y dólares). Vinculamos los objetos. Y grabamos la billetera. Cuando se registre un usuario, se le crean el usuario, la persona, la billetera y las cuentas.

--- src/main/java/ar/com/ada/api/billeteravirtual/services/BilleteraService.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/services/BilleteraService.java
@@ -1,6 +1,10 @@
 package ar.com.ada.api.billeteravirtual.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.billeteravirtual.entities.Billetera;
+import ar.com.ada.api.billeteravirtual.repos.BilleteraRepository;
 
 @Service
 public class BilleteraService {
@@ -21,4 +25,11 @@ public class BilleteraService {
     /* 3. Metodo: consultar saldo 
     3.1-- recibir el id de la billetera y la moneda en la que esta la cuenta
     */
+
+    @Autowired
+    BilleteraRepository repo;
+
+    public void grabar(Billetera billetera){
+        repo.save(billetera);
+    }
 }
--- src/main/java/ar/com/ada/api/billeteravirtual/services/UsuarioService.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/services/UsuarioService.java
@@ -1,13 +1,17 @@
 package ar.com.ada.api.billeteravirtual.services;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.com.ada.api.billeteravirtual.entities.Billetera;
+import ar.com.ada.api.billeteravirtual.entities.Cuenta;
 import ar.com.ada.api.billeteravirtual.entities.Persona;
 import ar.com.ada.api.billeteravirtual.entities.Usuario;
 import ar.com.ada.api.billeteravirtual.repos.UsuarioRepository;
+import ar.com.ada.api.billeteravirtual.security.Crypto;
 
 @Service
 public class UsuarioService {
@@ -18,48 +22,69 @@ public class UsuarioService {
     @Autowired
     PersonaService personaService;
 
+    @Autowired
+    BilleteraService billeteraService;
+
     public Usuario buscarPorUsername(String username) {
-		return null;
-	}
-
-	public void login(String username, String password) {
-
-        //codigo
-
-	}
-     
-    /* 2. Metodo: Iniciar Sesion 
-    2.1-- recibe el username y la password
-    2.2-- vamos a validar los datos
-    2.3-- devolver un verdadero o falso
-    */
-
-    public Usuario crearUsuario(String nombre, int pais, int tipoDocumento, String documento, Date fechaNacimiento, String email, String password) {
-
-        /*1.Metodo: Crear Usuario
-    1.1-->Crear una Persona(setearle un usuario)
-    1.2-->crear un usuario
-    1.3-->Crear una billetera(setearle una persona)
-    1.4-->Crear una cuenta en pesos y otra en dolares*/
-
-    Persona persona = new Persona();
-
-    persona.setNombre(nombre);
-    persona.setPaisId(pais);
-    persona.setTipoDocumentoId(tipoDocumento);
-    persona.setDocumento(documento);  
-    persona.setFechaNacimiento(fechaNacimiento);  
-
-    Usuario usuario = new Usuario();
-    usuario.setUsername(email);
-    usuario.setEmail(email);
-    usuario.setPassword(password);
-
-    persona.setUsuario(usuario);
-
-    personaService.grabar(persona);
-
         return null;
+    }
+
+    public void login(String username, String password) {
+
+        // codigo
+
+    }
+
+    /*
+     * 2. Metodo: Iniciar Sesion 2.1-- recibe el username y la password 2.2-- vamos
+     * a validar los datos 2.3-- devolver un verdadero o falso
+     */
+
+    public Usuario crearUsuario(String nombre, int pais, int tipoDocumento, String documento, Date fechaNacimiento,
+            String email, String password) {
+
+        /*
+         * 1.Metodo: Crear Usuario 1.1-->Crear una Persona(setearle un usuario)
+         * 1.2-->crear un usuario 1.3-->Crear una billetera(setearle una persona)
+         * 1.4-->Crear una cuenta en pesos y otra en dolares
+         */
+
+        Persona persona = new Persona();
+
+        persona.setNombre(nombre);
+        persona.setPaisId(pais);
+        persona.setTipoDocumentoId(tipoDocumento);
+        persona.setDocumento(documento);
+        persona.setFechaNacimiento(fechaNacimiento);
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername(email);
+        usuario.setEmail(email);
+        usuario.setPassword(Crypto.encrypt(password, email));
+
+        persona.setUsuario(usuario);
+
+        personaService.grabar(persona);
+
+        Billetera billetera = new Billetera();
+
+        Cuenta pesos = new Cuenta();
+        pesos.setSaldo(new BigDecimal(0));
+        pesos.setMoneda("ARS");
+
+        billetera.agregarCuenta(pesos);
+
+        Cuenta dolares = new Cuenta();
+        dolares.setSaldo(new BigDecimal(0));
+        dolares.setMoneda("USD");
+
+        billetera.agregarCuenta(dolares);
+
+        persona.setBilletera(billetera);
+
+        billeteraService.grabar(billetera);
+
+        return usuario;
 
     }
 
