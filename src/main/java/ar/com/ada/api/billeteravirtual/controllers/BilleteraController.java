93711e5439397d2b25dd6fea06acd020d81389e5
Noelia Albornoz <albornoznoeliaunlam@gmail.com>
2020-07-02 04:00:09 -0300

    Se agregaron y completaron dos web methods para consultar saldos (uno para consultar el saldo de una cuenta particular y otro para consultar los saldos de todas las cuentas).

--- src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/controllers/BilleteraController.java
@@ -1,27 +1,66 @@
 package ar.com.ada.api.billeteravirtual.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.billeteravirtual.entities.Billetera;
+import ar.com.ada.api.billeteravirtual.entities.Cuenta;
+import ar.com.ada.api.billeteravirtual.models.response.SaldoResponse;
+import ar.com.ada.api.billeteravirtual.services.BilleteraService;
 
 @RestController
 public class BilleteraController {
 
-    /* WEB Method 1: consultar saldo: GET
-                    URL: billeteras/{id}/saldos
-    WEB Method 2: cargar saldo: POST
-                    URL: billeteras/{id}/recargas
-                    requestBody: {
-                        "moneda":
-                        "importe":
-                    }
-    WEB Method 3: enviar saldo: POST
-                    URL: billeteras/{id}/envios
-                    requestBody: {
-                        "moneda":
-                        "importe":
-                        "email":
-                        "motivo":
-                        "detalleDelMotivo":
-                    } 
-    */
+    @Autowired
+    BilleteraService billeteraService;
+
+    /*
+     * WEB Method 1: consultar saldo: GET URL: billeteras/{id}/saldos WEB Method 2:
+     * cargar saldo: POST URL: billeteras/{id}/recargas requestBody: { "moneda":
+     * "importe": } WEB Method 3: enviar saldo: POST URL: billeteras/{id}/envios
+     * requestBody: { "moneda": "importe": "email": "motivo": "detalleDelMotivo": }
+     */
+
+    @GetMapping("billeteras/{id}/saldos/{moneda}")
+    public ResponseEntity<?> consultarSaldo(@PathVariable Integer id, @PathVariable String moneda) {
+
+        SaldoResponse saldo = new SaldoResponse();
+
+        saldo.saldo = billeteraService.consultarSaldo(id, moneda);
+        saldo.moneda = moneda;
+
+        return ResponseEntity.ok(saldo);
+
+    }
+
+    @GetMapping("billeteras/{id}/saldos")
+    public ResponseEntity<List<SaldoResponse>> consultarSaldo(@PathVariable Integer id) {
+
+        Billetera billetera = new Billetera();
+
+        billetera = billeteraService.buscarPorId(id);
+
+        List<SaldoResponse> saldos = new ArrayList<>();
+
+        for (Cuenta cuenta : billetera.getCuentas()) {
+
+            SaldoResponse saldo = new SaldoResponse();
+
+            saldo.saldo = cuenta.getSaldo();
+            saldo.moneda = cuenta.getMoneda();
+
+            saldos.add(saldo);
+
+        }
+
+        return ResponseEntity.ok(saldos);
+
+    }
 
 }
